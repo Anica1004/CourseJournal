af4b73b5a8e544376f428934b62f1514e0819c53
anica <anicamok@gmail.com>
2023-01-30 14:51:17 -0800

    Added course model with fields

---
+++ src/main/model/Course.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Course {
+    private String courseName;
+    private String professorName;
+    private int credit;
+    private int year;
+    private int finalMark;
+    private int term;
+    private String courseSummary;
+
+
+
+
+
+
+
+
+
+}
--- src/main/model/Student.java
+++ src/main/model/Student.java
@@ -1,5 +1,5 @@
 package model;
 
-public class MyModel {
+public class Student {
     // delete or rename this class!
 }
