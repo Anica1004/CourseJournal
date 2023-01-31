92563433a17fb12f31e60c800b838146ce695a5f
anica <anicamok@gmail.com>
2023-01-30 17:52:23 -0800

    Added student model

--- src/main/model/Course.java
+++ src/main/model/Course.java
@@ -1,5 +1,10 @@
 package model;
 
+
+// The class represents a "course" having a course name, the professor that taught
+// the course, course credit, the year and term (1 or 2) that the user has taken the course
+// the final mark that the user has gotten, and a short description of the course.
+
 public class Course {
     private String courseName;
     private String professorName;
@@ -10,11 +15,48 @@ public class Course {
     private String courseSummary;
 
 
+    // EFFECTS: constructs a course with a course name, professor name, number of credit,
+    //          the year taken, final mark, term, and course description
 
+    public Course(String courseName, String professorName, int credit, int year, int finalMark,
+                  int term, String courseSummary) {
+        this.courseName = courseName;
+        this.professorName = professorName;
+        this.credit = credit;
+        this.year = year;
+        this.finalMark = finalMark;
+        this.term = term;
+        this.courseSummary = courseSummary;
+    }
 
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
 
+    }
 
+    public void setProfessorName(String professorName) {
+        this.professorName = professorName;
+    }
 
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setFinalMark(int finalMark) {
+        this.finalMark = finalMark;
+    }
+
+    public void setTerm(int term) {
+        this.term = term;
+    }
+
+    public void setCourseSummary(String newSummary) {
+        this.courseSummary = newSummary;
+    }
 
 
 }
--- src/main/model/Student.java
+++ src/main/model/Student.java
@@ -1,5 +1,41 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+// This represents the student (user) with student's name, university name,
+// list of first, second, third, and fourth year courses, and list of all courses
+// that the student has taken in the university
+
 public class Student {
-    // delete or rename this class!
+    private String studentName;
+    private ArrayList<Course> firstYearCourses;
+    private ArrayList<Course> secondYearCourses;
+    private ArrayList<Course> thirdYearCourses;
+    private ArrayList<Course> fourthYearCourses;
+    private List<List<Course>> listOfCourses = new ArrayList<>();
+
+    public Student() {
+      studentName = "user";
+      firstYearCourses = new ArrayList<>();
+      secondYearCourses = new ArrayList<>();
+      thirdYearCourses = new ArrayList<>();
+      fourthYearCourses = new ArrayList<>();
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public void setFirstYearCourses(Course course) {
+        firstYearCourses.add(course);
+    }
+
+
+
+
+
+
+
+
 }
