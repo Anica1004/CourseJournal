3936a9ca0cb35a8b010fc8bca552a7c120f006f5
anica <anicamok@gmail.com>
2023-03-05 07:57:09 -0800

    Finished putting the tests

--- src/test/model/JsonSaverTest.java
+++ src/test/model/JsonSaverTest.java
@@ -55,12 +55,12 @@ public class JsonSaverTest {
     void testWriteStudentWithNoCourse() {
         try {
             Student userTest = new Student();
-            JsonSaver saver = new JsonSaver("./data/studentWithNoCourse");
+            JsonSaver saver = new JsonSaver("./data/studentWriteWithNoCourse.json");
             saver.open();
             saver.write(userTest);
             saver.close();
 
-            JsonLoader reader = new JsonLoader("./data/studentWithNoCourse");
+            JsonLoader reader = new JsonLoader("./data/studentWriteWithNoCourse.json");
                 Student testUser = reader.read();
                 assertEquals(emptyList, testUser.getFirstYearCourses());
                 assertEquals(emptyList, testUser.getSecondYearCourses());
@@ -76,14 +76,53 @@ public class JsonSaverTest {
     void testWriteStudentWithCourse() {
         try {
             Student userTest = new Student();
-            wr.addThingy(new Thingy("saw", Category.METALWORK));
-            wr.addThingy(new Thingy("needle", Category.STITCHING));
-            JsonWriter writer = new JsonWriter("./data/testWriterGeneralWorkroom.json");
-            writer.open();
-            writer.write(wr);
-            writer.close();
-        }
+            Course firstCourse = new Course("cpsc 110", "gregor", 4, 1, 94,
+                    1, 10, "For Test 1");
+            Course secondCourse = new Course("cpsc 210", "steven wolfman", 4, 2, 100,
+                    2, 10, "For Test 2");
+            Course thirdCourse = new Course("cpsc 310", "amy", 4, 3, 100,
+                    2, 10, "For Test 3");
+            Course fourthCourse = new Course("cpsc 410", "anica", 4, 4, 100,
+                    1, 10, "For Test 4");
+            userTest.addFirstYearCourses(firstCourse);
+            userTest.addSecondYearCourses(secondCourse);
+            userTest.addThirdYearCourses(thirdCourse);
+            userTest.addFourthYearCourses(fourthCourse);
+            JsonSaver saver = new JsonSaver("./data/studentWriteWithCourse.json");
+            saver.open();
+            saver.write(userTest);
+            saver.close();
 
+            JsonLoader reader = new JsonLoader("./data/studentWriteWithCourse.json");
+            userTest = reader.read();
+            Course userFirstYearCourse = userTest.getFirstYearCourses().get(0);
+            Course userSecondYearCourse = userTest.getSecondYearCourses().get(0);
+            Course userThirdYearCourse = userTest.getThirdYearCourses().get(0);
+            Course userFourthYearCourse = userTest.getFourthYearCourses().get(0);
+            compareCourses(userFirstYearCourse, firstCourse);
+            compareCourses(userSecondYearCourse, secondCourse);
+            compareCourses(userThirdYearCourse, thirdCourse);
+            compareCourses(userFourthYearCourse, fourthCourse);
+
+
+        } catch (IOException e) {
+            fail("The file was not able to be read properly");
+        }
+    }
+
+
+    private void compareCourses(Course userCourse, Course expectedCourse){
+        assertEquals(userCourse.getCourseSummary(), expectedCourse.getCourseSummary());
+        assertEquals(userCourse.getCourseName(), expectedCourse.getCourseName());
+        assertEquals(userCourse.getYear(), expectedCourse.getYear());
+        assertEquals(userCourse.getRating(), expectedCourse.getRating());
+        assertEquals(userCourse.getCredit(), expectedCourse.getCredit());
+        assertEquals(userCourse.getFinalMark(), expectedCourse.getFinalMark());
+        assertEquals(userCourse.getTerm(), expectedCourse.getTerm());
+        assertEquals(userCourse.getProfessorName(), expectedCourse.getProfessorName());
+
+
+    }
 
 
 
